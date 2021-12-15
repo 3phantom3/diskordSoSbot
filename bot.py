2fb2e2553973fab6af4a84058d9ff1c51b894d0c
sergey <3phantom3@gmail.com>
2021-12-16 02:44:14 +0300

    fix

--- bot.py
+++ bot.py
@@ -28,26 +28,25 @@ function "$conf 15-12-2021 15:00" -- set trap time')
         msg = ' '.join(message.content.split()[1:])
         time_input = datetime.datetime.strptime(msg,'%d-%m-%Y %H:%M')
         basa[str(message.channel.id)] = time_input
-        await message.channel.send(msg)
-#
-# async def background_task():
-#     for i in basa:
-#         timesave = datetime.datetime.strptime(basa[i], '%d-%m-%Y %H:%M')
-#         delta = datetime.datetime.utcnow() - timesave
-#         if delta == datetime.timedelta(hours=1):
-#             await client.get_channel(int(i)).sendgit ('''```🗓NOTIFICATION:
-# Trap is in 1 hour.``` @everyone''')
-#         elif delta == datetime.timedelta(minutes=15):
-#             await client.get_channel(int(i)).send('''```🗓NOTIFICATION:
-# Trap is in 15 minutes.``` @everyone''')
-#         elif delta == datetime.timedelta(minutes=5):
-#             await client.get_channel(int(i)).send('''```🗓NOTIFICATION:
-# Trap is in 5 minutes. Recall all your troops``` @everyone''')
-#         elif delta == 0:
-#             await client.get_channel(int(i)).send('''```🗓NOTIFICATION:
-# It's Trap Time!``` @everyone''')
-#             timesave += datetime.timedelta(days=2)
-#             basa[i] = timesave
-#
-# client.loop.create_task(background_task())
+
+async def background_task():
+    for i in basa:
+        timesave = datetime.datetime.strptime(basa[i], '%d-%m-%Y %H:%M')
+        delta = datetime.datetime.utcnow() - timesave
+        if delta == datetime.timedelta(hours=1):
+            await client.get_channel(int(i)).sendgit ('''```🗓NOTIFICATION:
+Trap is in 1 hour.``` @everyone''')
+        elif delta == datetime.timedelta(minutes=15):
+            await client.get_channel(int(i)).send('''```🗓NOTIFICATION:
+Trap is in 15 minutes.``` @everyone''')
+        elif delta == datetime.timedelta(minutes=5):
+            await client.get_channel(int(i)).send('''```🗓NOTIFICATION:
+Trap is in 5 minutes. Recall all your troops``` @everyone''')
+        elif delta == 0:
+            await client.get_channel(int(i)).send('''```🗓NOTIFICATION:
+It's Trap Time!``` @everyone''')
+            timesave += datetime.timedelta(days=2)
+            basa[i] = timesave
+
+client.loop.create_task(background_task())
 client.run(my_secret)
